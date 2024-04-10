6a0bc92bb07d6d6ee2ed1d82b90dca8e5b25cdab
Nicholas Amemazior <amemaziornicholas77@gmail.com>
2024-04-10 19:53:10 +0000

    fixed engine lighting issues

--- Assets/Scripts/Back Titration Scripts/ScaleReadingScript.cs
+++ Assets/Scripts/Back Titration Scripts/ScaleReadingScript.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
-using UnityEditor.PackageManager.UI;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
